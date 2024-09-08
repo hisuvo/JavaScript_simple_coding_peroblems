8f729817c731566e9ffa3ed0419bf3b472894c62
suvo <suvodatta72@gmail.com>
2024-09-08 14:55:56 +0600

    task one solution done

--- problem-2/task/taslk-1.js
+++ problem-2/task/taslk-1.js
@@ -1,5 +1,22 @@
 /*
-	Task -1:
-		Find the lowest number in the array below.
-		const heights2 = [167, 190, 120, 165, 137];
+Task -1:
+Find the lowest number in the array below.
+const heights2 = [167, 190, 120, 165, 137]
 */
+
+// Solution :
+
+const heights = [167, 190, 120, 165, 137]
+
+function lowestNumberFinder (numbers) {
+	let min = numbers[0]
+	for (var i = numbers.length - 1; i >= 0; i--) {
+		if(numbers[i] < min){
+			min = numbers[i]
+		}
+	}
+	return min
+} 
+
+let lowerHeights = lowestNumberFinder(heights)
+console.log("Lower heights Number is:", lowerHeights)
