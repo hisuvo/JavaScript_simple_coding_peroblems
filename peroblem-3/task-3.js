7abcb151600f72f3c49fe642436da179921eebb9
suvo <suvodatta72@gmail.com>
2024-09-09 16:52:55 +0600

    task-3 done

---
+++ peroblem-3/task-3.js
@@ -0,0 +1,24 @@
+// Task -3:
+
+
+function deletedInvalid (array) {
+	if(!Array.isArray(array)) {
+		return "Invalid Array"
+	}
+
+	let newArray = [];
+
+	for(let i = 0; i < array.length; i++) {
+		let item = array[i];
+		if(typeof item === "number" && !isNaN(item)) {
+			newArray.push(item)
+		}
+	}
+
+	return newArray
+}
+
+
+let inputArray = [1, 2, NaN, undefined]
+
+console.log(deletedInvalid(inputArray))
